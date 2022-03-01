32c0a8d9b9f2eb94b42b57a838f67293b0ef67b5
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2022-02-28 22:55:37 -0500

    Get to Know and Use Laravel Jetstream

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Welcome You Again
+    </div>
+</template>
+<script>
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        components: {
+            //
+        },
+
+        props: {
+            canLogin: Boolean,
+            canRegister: Boolean,
+        }
+    })
+</script>
